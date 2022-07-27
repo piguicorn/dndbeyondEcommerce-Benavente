55b0378264c14b3275a25eb95418d24138e81f5b
Logan Ben <hello.piguicorn@gmail.com>
2022-07-27 11:41:27 +0200

    changed folder structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 /* components */
-import App from './App';
+import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
